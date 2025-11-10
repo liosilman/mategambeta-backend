90f37dd7f2b322193ce527b88b6c7642536a6be9
lionDev <liosilman02@gmail.com>
2025-11-10 17:09:53 -0300

    vercel fix

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
@@ -41,7 +42,9 @@ app.use((err, req, res, next) => {
   })
 })
 
-const PORT = process.env.PORT || 5000
-app.listen(process.env.PORT || 3000, () => console.log(`Servidor corriendo en el puerto ${process.env.PORT || 3000}`))
+// ⛔️ Eliminar esto:
+// const PORT = process.env.PORT || 5000
+// app.listen(PORT, ...)
 
-
+// ✅ Agregar esto:
+export default app
